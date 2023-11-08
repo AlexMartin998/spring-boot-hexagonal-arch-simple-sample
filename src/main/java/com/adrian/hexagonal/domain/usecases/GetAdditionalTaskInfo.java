d6f8d596787dacbd48f0e9db9c5215329d7ea079
Adrian <alextravc.342@gmail.com>
2023-11-08 11:21:50 -0500

    [ADD]: GetAdditionalTaskInfo use case domain

---
+++ src/main/java/com/adrian/hexagonal/domain/usecases/GetAdditionalTaskInfo.java
@@ -0,0 +1,8 @@
+package com.adrian.hexagonal.domain.usecases;
+
+import com.adrian.hexagonal.domain.models.AdditionalTaskInfo;
+
+
+public interface GetAdditionalTaskInfo {
+    AdditionalTaskInfo getAdditionalTaskInfo(Long taskId);
+}
